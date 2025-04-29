7b2469c8fbcbd535c2614ce598779bd073b35e81
schmos <anugraham.mail@gmail.com>
2025-04-29 11:53:40 -0500

    Update index.js to match stremio version 1.0.0

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const express = require("express");
 const addonInterface = require("./addon");
 
 const app = express();
-const port = process.env.PORT || 7000;
+const PORT = process.env.PORT || 10000;
 
-app.use("/", addonInterface.getRouter());
+app.get("/manifest.json", (req, res) => {
+    res.send(addonInterface.manifest);
+});
 
-app.listen(port, () => {
-  console.log(`Stremio addon running on port ${port}`);
+app.get("/catalog/:type/:id/:extra?.json", (req, res) => {
+    addonInterface.catalog(req, res);
+});
+
+app.get("/stream/:type/:id.json", (req, res) => {
+    addonInterface.stream(req, res);
+});
+
+app.listen(PORT, () => {
+    console.log(`Stremio addon running on port ${PORT}`);
 });
