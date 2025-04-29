a2b839d7dae11c0e2d8eaeff838be7b295ffe68e
schmos <anugraham.mail@gmail.com>
2025-04-29 14:05:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ const addonInterface = require("./addon");
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-// Manifest route
+// Manifest
 app.get("/manifest.json", (req, res) => {
     res.send(addonInterface.manifest);
 });
 
-// Catalog route (no .json in the route path to avoid parsing issues)
+// Catalog
 app.get("/catalog/:type/:id/:extra?", (req, res) => {
     addonInterface.catalog(req, res);
 });
 
-// Stream route (same: remove .json from route definition)
+// Stream
 app.get("/stream/:type/:id", (req, res) => {
     addonInterface.stream(req, res);
 });
 
-// Start server
+// Listen
 app.listen(PORT, () => {
     console.log(`Stremio addon running on port ${PORT}`);
 });
